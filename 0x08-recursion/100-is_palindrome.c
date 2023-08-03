16244a5407adf16ef035b4f7600226bb4ba0fcac
NIGUSALEX <aalemnguss@gmail.com>
2023-08-02 23:08:32 -0700

    advanced correction

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,9 +15,23 @@ int last_index(char *s)
 
 	return (n);
 }
+/
+ int is_palindrome(char *s)
+{
+	int end = last_index(s);
 
-/**
- * is_palindrome - check if a string is a palindrome
- * @s: string to check
- * Return: 0 or 1
- */
+	return (check(s, 0, end - 1, end % 2));
+}
+
+
+
+int check(char *s, int start, int end, int pair)
+{
+
+	if ((start == end && pair != 0) || (start == end + 1 && pair == 0))
+		return (1);
+	else if (s[start] != s[end])
+		return (0);
+	else
+		return (check(s, start + 1, end - 1, pair));
+}
